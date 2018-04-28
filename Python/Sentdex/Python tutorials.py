b024ffd92502fd3d18ff39d3d7eb5589e337d15f
h0ssein <h0ssein2011@yahoo.com>
2018-04-28 20:27:02 +0430

    this is ver 0.1

---
+++ Python/Sentdex/Python tutorials.py
@@ -0,0 +1,10 @@
+Python 2.7.13 |Anaconda 4.0.0 (64-bit)| (default, Dec 19 2016, 13:29:36) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import sys
+>>> import time
+>>> 
+>>> print(sys.version)
+2.7.13 |Anaconda 4.0.0 (64-bit)| (default, Dec 19 2016, 13:29:36) [MSC v.1500 64 bit (AMD64)]
+>>> print(time.sleep(7))
+None
+>>> 

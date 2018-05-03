6e5184c5c079d4137ea12083ec6403f394343b76
h0ssein <h0ssein2011@yahoo.com>
2018-05-03 21:38:56 +0430

    ver 1.5 5may

---
+++ Python/Sentdex/Basic Functions.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 03 21:33:46 2018
+
+@author: Hossein
+"""
+
+# this is Function tutorial
+
+def simple_func():
+    
+    pass
+
+def simple_calculator(x,y):
+    
+    sums=x+y
+    multiply=x*y
+    dif=x-y
+    divide=x/y
+    
+    print(sums,multiply,dif,divide)
+    
+simple_func()
+
+simple_calculator(5,700)
+    
+    
---
+++ Python/Sentdex/if & elifs.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 03 20:25:46 2018
+
+@author: Hossein
+"""
+# this is if and elif tutorial
+
+x=5
+
+y=6
+
+z=52
+
+if x>y :
+    
+    print('x is greater than y')
+    
+elif x > z:
+    print('x  is not less than z')
+    
+elif x <52:
+    print('x is less than55')    
+    
+else:
+    print('if fucked!')
+    
+
+    
+    

8a4465503b91c1bc00ff23c18889fa95550620d6
h0ssein <h0ssein2011@yahoo.com>
2018-05-08 21:19:55 +0430

    ver 1.10 8 may

---
+++ Python/Sentdex/Simple class.py
@@ -0,0 +1,23 @@
+# this is a simple class
+
+class calculator(x,y):
+
+    def multiply():
+        multi=x*y
+        print(multi)
+        
+    def deviation():
+
+        print(x/y)
+
+    def substract():
+
+        print(x-y)
+
+    def power():
+
+        print(x**y)
+
+calculator.multiply(3,7)
+
+        

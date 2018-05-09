777f233d166bfe86892bbbe37fdeae1ed0165d56
h0ssen2011 <h0ssein2011@yahoo.com>
2018-05-09 15:52:26 +0330

    ver 1.11 9 may

---
+++ Python/lpthw/ex0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 15:29:14 2018
+
+@author: Snapp
+"""
+
+#Execrice 0
+
+fruits=['banana','orange','kiwi','melon','orange','kiwi']
+
+fruits.count('orange')
+
+fruits.index('orange')

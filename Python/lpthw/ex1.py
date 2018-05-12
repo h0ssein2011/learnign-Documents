8f9afa2c6b4df0c6a9a645cdd8f26dfdb87ad4c5
h0ssen2011 <h0ssein2011@yahoo.com>
2018-05-12 14:48:27 +0330

    Ver 1.12

---
+++ Python/lpthw/ex1.py
@@ -0,0 +1,10 @@
+print('Hello world')
+print("Hello again")
+print("I like typing this")
+print('this is fun')
+print('! yay ! printing')
+print('I would rather than you not')
+print('I "said" do not touch this ')
+
+
+print('\n')
---
+++ Python/lpthw/ex3.py
@@ -0,0 +1,7 @@
+# this is comment exercise
+
+# this would not print
+
+print('but this will be print')
+
+print(" what about # this!")

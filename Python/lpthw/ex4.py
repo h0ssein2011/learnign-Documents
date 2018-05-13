0f94625b529af00c38b049f8441a92d9ce32e374
h0ssen2011 <h0ssein2011@yahoo.com>
2018-05-13 12:16:45 +0330

    ver 1.12

---
+++ Python/lpthw/ex4.py
@@ -0,0 +1,27 @@
+
+# this is exce4 math tutorial
+
+
+print("I will now count on my chickens: ")
+
+print("hens:",25+30/6)
+
+print("Roosters:" ,100 - 25 *3 %4)
+
+print("Now I will count the eggs:")
+print(3+2-5+4 % 2 -1 /4+6)
+
+print("is it True that:3+2 < 5-7?")
+print(3+2 < 5-7)
+
+print("What is 3+2?",3+2)
+print("wHat is 5-7?",5-7)
+
+print("Oh why this is False.")
+
+print("What about some more")
+
+print("Is it greater?", 5 >=2)
+print("is it greater or equal?" , 5>=2)
+
+print("is it less or equal?" , 5<= -2)

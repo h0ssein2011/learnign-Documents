e5b462c2d9bd278bcecf0364ec4da301835f52ee
h0ssen2011 <h0ssein2011@yahoo.com>
2018-05-14 16:14:13 +0330

    Ver1.14 14 may

---
+++ Python/lpthw/ex8.py
@@ -0,0 +1,8 @@
+# this is formatting tutorials
+
+formatter="{}  {}  {}  {}"
+print(formatter.format(1,2,3,4))
+print(formatter.format("one","two","three","four"))
+print(formatter.format(True,False,True,False))
+print(formatter.format(formatter,formatter,formatter,formatter))
+print(formatter.format("try your","own text","mayeb a poem","or a song about fear"))

a87132dcb05fce8b2b02eb5cfddae1d1c364c717
h0ssen2011 <h0ssein2011@yahoo.com>
2018-05-15 12:26:19 +0330

    Ver 1.16 15 may

---
+++ Python/lpthw/ex11.py
@@ -0,0 +1,10 @@
+# this input tutorial
+
+print("How old are you?",end=' ')
+age=input()
+print("How tall are you?" , end=' ')
+height=input()
+print("How much do you weight?", end=' ')
+weight=input()
+
+print(f"so you are {age} years old and {height} inch and {weight} heavy")
---
+++ Python/lpthw/ex12.py
@@ -0,0 +1,8 @@
+# this is continous of previous input tutorial
+
+age =input("How old are you?")
+
+weight=input("How much are your weight?")
+height=input("How tall are you?")
+
+print(f"So you are {age} years old and {height} cm and {weight} km")

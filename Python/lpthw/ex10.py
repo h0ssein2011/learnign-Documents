14ec62492cf8621e3b1187324ea751ce6773654e
h0ssein <h0ssein2011@yahoo.com>
2018-05-15 06:14:29 +0430

    ver 1.15 morning 15 may

---
+++ Python/lpthw/ex10.py
@@ -0,0 +1,22 @@
+# this is especial string tutorials
+
+tabby_cat="\t I'am a tabby cat"
+persian_cat="I'm a split \non a line"
+
+backslash_cat="I'm a \\ a \\ cat"
+
+fat_cat=""" i will do a list:
+\t* food fat_cat
+\t*fishes
+\t*mouse
+\t*catnip\n\t*Grass
+
+
+ """
+
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(" ")
---
+++ Python/lpthw/ex9.py
@@ -0,0 +1,18 @@
+
+# this is new string tutorials
+
+# you will see strange stuff so dont panic!
+
+days="Sat Sun Mon Tue Wed Thr Fri"
+
+months="Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here is the days:",days)
+print("here is the months:",months)
+
+print(""" this is tripple quote so
+you can type everything Here
+as much as line that you want
+.....
+""")
+print(" ")

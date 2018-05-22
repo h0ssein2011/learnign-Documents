9340b9d5340bab4281e1335991456a6fa05ef76b
h0ssein <h0ssein2011@yahoo.com>
2018-05-22 06:21:24 +0430

    ver 1.20 morning 22may

--- Python/lpthw/ex25.py
+++ Python/lpthw/ex25.py
@@ -1,6 +1,7 @@
 
-
+""" this tutorial will show how use functions"""
 def break_words(stuff):
+    """ this function split words for us """
     words=stuff.split(' ')
     return(words)
 
@@ -15,7 +16,7 @@ def print_last_word(words):
     word=words.pop(-1)
     print(word)
 
-def sort_senternce(sentence):
+def sort_sentence(sentence):
     words=break_words(sentence)
     return(sorted_word(words))
 
@@ -25,6 +26,6 @@ def print_first_and_last(sentence):
     print_last_word(words)
 
 def print_first_and_last_sorted(sentence):
-    words=sort_senternce(sentence)
+    words=sort_sentence(sentence)
     print_first_word(words)
     print_last_word(words)

33adf2863b52ef314fc6cdc31aba260314b2f96b
h0ssen2011 <h0ssein2011@yahoo.com>
2018-05-22 15:37:38 +0330

    ver 1.21 evening 22 may

---
+++ Python/lpthw/ex29.py
@@ -0,0 +1,27 @@
+# this tutorial is related to What_if
+
+people=50
+cats=30
+dogs=15
+
+if people < cats:
+    print("too many dogs world is doomed")
+
+if people > cats:
+    print("Not many cats! world is saved")
+
+if people < dogs :
+    print("world is drooled")
+
+if people > dogs:
+    print("The world is dry")
+
+dogs +=5
+if people >= dogs:
+    print("people are greater or equal to dogs")
+
+if people <= dogs:
+    print("people are less than or equal to dogs")
+
+if people == dogs:
+    print("people are dogs")
---
+++ Python/lpthw/ex30.py
@@ -0,0 +1,25 @@
+# this is if $ elif tutorial
+
+people =100
+cars=20
+trucks=30
+
+if people > cars:
+    print("we should take care of cars")
+elif people < cars:
+    print("we should not take care of cars")
+else:
+    print("we cant decide")
+
+if trucks > cars:
+    print("there are too many trucks")
+elif trucks < cars:
+    print("meybe we should take trucks")
+else:
+    print("we still cant decide")
+
+if people > trucks:
+    print("lets take the trucks")
+
+else:
+    print("let's stay at home")

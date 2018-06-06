110b7fff2fe75d03bdeb10f126ab10f8f6a4befb
h0ssein <h0ssein2011@yahoo.com>
2018-06-06 23:31:52 +0430

    ver 1.31 evening 6jun

--- Python/lpthw/ex45.py
+++ Python/lpthw/ex45.py
@@ -1,33 +1,37 @@
+# import needed library
 from random import randint
+#choose rooms before start
 rooms=['dark_room','gold_room','rand_room','win_room']
 
-class dark_room(object):
+#Dark Room is the first
+class DarkRoom(object):
     def enter():
         print("start dark_room")
-
-class gold_room(object):
+#gold room that finished room
+class GoldRoom(object):
     def get_gold():
         print("hey you get golds")
         return "Finished you win"
-
-class rand_room(object):
+#this is just a fake room that pass you another room! haha
+class RandRoom(object):
     def choose():
         which_room=int(input("choose number between 0 and 3:"))
         if which_room ==2 :
             which_room=int(randint(0,3))
         else:
             return rooms[which_room]
-
-class win_room(object):
+# this is the last room
+class WinRoom(object):
     def win():
         print("Hey you win you can earn money!")
 
+#let's play
 which_room=int(input("choose number between 0 and 3:"))
 if which_room == 0:
-    dark_room.enter()
+    DarkRoom.enter()
 elif which_room == 1:
-    gold_room.get_gold()
+    GoldRoom.get_gold()
 elif which_room == 2:
-    rand_room.choose()
+    RandRoom.choose()
 else:
-    win_room.win()
+    WinRoom.win()

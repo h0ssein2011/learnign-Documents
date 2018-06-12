3b25b380c2940beda8fe2843b01a1fc8438935e7
h0ssen2011 <h0ssein2011@yahoo.com>
2018-06-12 17:10:03 +0330

    ver 0.2 evening 12 june

--- Python/Python cookbook/ch1.py
+++ Python/Python cookbook/ch1.py
@@ -1,5 +1,29 @@
 p=(4,5)
 a,b=p
 
-a
-b
+
+
+print(a,b)
+
+data=['ACME',50,90.1,(2012,11,12)]
+
+name,shares,price,date=data
+
+print(name,shares,price,date)
+
+
+s='Hello'
+
+a,b,c,d,e=s
+
+print(a +'\n',b +'\n',c +'\n',d +'\n',e+ '\n')
+
+
+# probelm 2:unpacking
+from numpy import mean
+def drop_first_last(grades):
+    first,*middle,last = grades
+    return middle,mean(middle)
+
+grades=[1,4,5,1,8,9,12,36,89]
+drop_first_last(grades)

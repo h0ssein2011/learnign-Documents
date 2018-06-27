a65692bb494414dea2398867579ee2d6ea1c4646
h0ssein <h0ssein2011@yahoo.com>
2018-06-27 06:00:26 +0430

    ver 1.2 morning 27 june

--- Python/Python cookbook/ch2.py
+++ Python/Python cookbook/ch2.py
@@ -5,3 +5,20 @@ import re
 
 r=re.split(r'[;,\s]\s*',line)
 r
+
+fields =re.split(r'(;|,|\s*)',line)
+
+# start and end a text
+filename='spam.txt'
+
+filename.startswith('spam')
+filename.endswith('.txt')
+
+name,format=filename.split('.')
+
+#mulitplie choose
+import os
+filename=os.listdir()
+filename
+
+[name for name in filename if name.endswith(('.py','.h'))]

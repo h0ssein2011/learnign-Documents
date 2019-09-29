f0a89c704f0c1309ea1f890b55348ee6f5688a00
h0ssen2011 <h0ssein2011@yahoo.com>
2019-09-29 13:21:58 +0330

    Nested Lists in python

---
+++ Python/hackerrank_com/Nested Lists.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n=int(input())
+    students=[[input(), input()] for _ in range(n) ]
+
+
+second_highest = sorted(list(set([marks for name, marks in students])))[1]
+print('\n'.join([a for a,b in sorted(students) if b == second_highest]))

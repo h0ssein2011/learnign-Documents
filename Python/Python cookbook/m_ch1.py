96b6192f01af5c1f9bdd17ea7fa71b018494591c
h0ssein <h0ssein2011@yahoo.com>
2019-10-09 23:22:20 +0330

    unpack an iterbale

--- Python/Python cookbook/m_ch1.py
+++ Python/Python cookbook/m_ch1.py
@@ -1,5 +1,23 @@
 #unpack a list
-my_list=['asm',1,3,5,7]
-txt,number=my_list[0],my_list[1:]
+my_list=['asm',1,3,5,7,(2019,8,7)]
+
+txt,number,date=my_list[0],my_list[1:-1],my_list[-1]
+
+
+
 print(txt)
 print(number)
+print(date)
+
+
+#unpack a huge list
+import numpy as np
+ls=[i for i in range(100)]
+print(ls)
+
+
+def get_middle(x):
+    first,*middle,last=x
+    print(middle )
+
+get_middle(ls)

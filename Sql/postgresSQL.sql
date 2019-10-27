d516a9652ef824ec16cc70c750244d0566900bb1
h0ssen2011 <h0ssein2011@yahoo.com>
2019-10-27 11:46:27 +0330

    update some advanced queries

--- Sql/postgresSQL.sql
+++ Sql/postgresSQL.sql
@@ -16,3 +16,43 @@ UNION
   select name
   from
   cd.facilities
+
+/subquery
+  select firstname, surname, joindate
+  	from cd.members
+  	where joindate =
+  		(select max(joindate)
+
+
+/case sensetive search
+select * from cd.facilities
+where name ILIKE 'tennis%'
+
+select memid, telephone from cd.members where telephone ~ '[()]';
+
+
+/REPLACE characters
+select memid, translate(telephone, '-() ', '') as telephone
+    from cd.members
+    order by memid;
+
+
+/join
+select mem.memid,mem.surname,mem.telephone,bking.slots
+
+from cd.members mem
+
+left join
+
+cd.bookings bking
+
+ON mem.memid=bking.memid
+where bking.slots=4
+
+
+# return first 5 rows
+select * from
+cd.members
+order by joindate DESC
+fetch first
+5 rows only

e46a384637604ab8f0b849d0fed768e2dffc8c7a
h0ssen2011 <h0ssein2011@yahoo.com>
2019-11-02 14:29:28 +0330

    update pstgresql learning

--- Sql/postgresSQL.sql
+++ Sql/postgresSQL.sql
@@ -79,7 +79,7 @@ from
 cd.facilities
 
 ----window fucntions in postgresql
-----http:----www.postgresqltutorial.com--postgresql-window-function--
+--http:----www.postgresqltutorial.com--postgresql-window-function--
 
 
 --join
@@ -124,3 +124,109 @@ cd.facilities fac
 		on bks.facid=fac.facid
 where fac.facid in (0,1)
 order by member
+
+
+--case https://pgexercises.com/questions/joins/threejoin2.html
+select mems.firstname || ' ' || mems.surname as member,
+	facs.name as facility,
+	case
+		when mems.memid = 0 then
+			bks.slots*facs.guestcost
+		else
+			bks.slots*facs.membercost
+	end as cost
+        from
+                cd.members mems
+                inner join cd.bookings bks
+                        on mems.memid = bks.memid
+                inner join cd.facilities facs
+                        on bks.facid = facs.facid
+        where
+		bks.starttime >= '2012-09-14' and
+		bks.starttime < '2012-09-15' and (
+			(mems.memid = 0 and bks.slots*facs.guestcost > 30) or
+			(mems.memid != 0 and bks.slots*facs.membercost > 30)
+		)
+order by cost desc;
+
+
+-- subquery
+select concat(firstname,' ',surname) as member,
+(
+ select  concat(firstname,' ',surname) as recommender
+  from cd.members recs
+  where recs.memid=mems.recommendedby
+  )
+
+from cd.members mems
+order by member
+
+--month in postgresql
+select facid,extract(month from starttime) as month ,sum(slots) as "Total Slots"
+
+from cd.bookings
+where
+		starttime >= '2012-01-01'
+		and starttime < '2013-01-01'
+group by facid,month
+order by facid,month
+
+--case & aggregate
+select fac.name,
+sum( slots *
+case
+	when bks.memid=0 then fac.guestcost
+	else fac.membercost
+
+end ) revenue
+
+from cd.facilities fac
+inner join cd.bookings bks
+on bks.facid = fac.facid
+
+group by fac.name
+order by revenue
+
+
+-- select all rows counts
+select (select count(*) from cd.members),firstname,surname
+
+from cd.members
+order by joindate
+
+--row number in postgresql
+select ROW_NUMBER() over() as row_number ,firstname,surname
+from cd.members
+order by joindate
+
+
+--ntile
+select name, case when class=1 then 'high'
+		when class=2 then 'average'
+		else 'low'
+		end revenue
+	from (
+		select facs.name as name, ntile(3) over (order by sum(case
+				when memid = 0 then slots * facs.guestcost
+				else slots * membercost
+			end) desc) as class
+		from cd.bookings bks
+		inner join cd.facilities facs
+			on bks.facid = facs.facid
+		group by facs.name
+	) as subq
+order by class, name;
+
+
+--limit agg fucntion
+select name, revenue from (
+	select facs.name, sum(case
+				when memid = 0 then slots * facs.guestcost
+				else slots * membercost
+			end) as revenue
+		from cd.bookings bks
+		inner join cd.facilities facs
+			on bks.facid = facs.facid
+		group by facs.name
+	) as agg where revenue < 1000
+order by revenue;

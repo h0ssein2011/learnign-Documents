5352537144d3984395eb88878d3a0f622e972795
h0ssen2011 <h0ssein2011@yahoo.com>
2019-11-28 11:49:22 +0330

    update rename files

--- Python/Practical codes/rename_files.py
+++ Python/Practical codes/rename_files.py
@@ -1,15 +1,17 @@
 
 import os
 # change the dicnaries
-os.chdir('C:/Users/Hossein/Desktop/dl1/W1')
-#get all filenames and rename based on the this 
+os.chdir('D:/Kaggle competition')
+#get all filenames and rename based on the this
 for file in os.listdir():
 
 
     f=open(file,'r')
-    src=file[f.name.find('(')+1:f.name.find(')')]
+    # src=file[f.name.find('(')+1:f.name.find(')')]
+    src=file[:f.name.find('How')].strip()
     extention=file[f.name.find('.'):]
     f.close()
-    new_name=src+'_'+file[:f.name.find('(')]+extention
-
+    # new_name=src+'_'+file[:f.name.find('(')]+extention
+    new_name=src + extention
+    #print(new_name)
     os.rename(file,new_name)

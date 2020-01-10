1154df7ef2c5315358cea670b3b5b70d44ba929d
h0ssein2011 <h0ssein2011@yahoo.com>
2020-01-10 13:46:47 +0330

    the central theorem in practice(uniform/exponentioal and poisson distribution)

--- Statistics/The central limit theorem.py
+++ Statistics/The central limit theorem.py
@@ -8,12 +8,37 @@ import pandas as pd
 
 unif_dist = np.random.uniform(0,100,1000)
 
-means_unif_dist = [np.mean(sample) for sample in choices(unif_dist,k=100)]
+means_unif_dist = []
+for i in range(10,1100 ,10):
+    means_unif_dist.append(np.mean(choices(unif_dist,k=i)))
 # print(means_unif_dist)
 fig ,axs = plt.subplots(2,1)
-axs[0].hist(unif_dist,bins=100)
-axs[1].hist(means_unif_dist,bins=100)
-
+fig.suptitle('uniform dist vs sample mean distribution')
+axs[0].hist(unif_dist)
+axs[1].hist(means_unif_dist)
 plt.show()
 
-# print(len(unif_dist))
+
+exp_dist = np.random.exponential(size= 1000)
+means_exp_dist = []
+for i in range(10,1100 ,10):
+    means_exp_dist.append(np.mean(choices(exp_dist,k=i)))
+
+fig ,axs = plt.subplots(2,1)
+fig.suptitle('exponentional dist vs sample mean distribution')
+axs[0].hist(exp_dist)
+axs[1].hist(means_exp_dist)
+plt.show()
+
+
+
+pois_dist = np.random.poisson(size=1000)
+means_pois_dist = []
+for i in range(10,1100 ,10):
+    means_pois_dist.append(np.mean(choices(pois_dist,k=i)))
+
+fig ,axs = plt.subplots(2,1)
+fig.suptitle('poisson dist vs sample mean distribution')
+axs[0].hist(pois_dist)
+axs[1].hist(means_pois_dist)
+plt.show()

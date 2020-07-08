b820b6e053dabac91dc612dc7540dd0d229b9d94
h0ssein2011 <h0ssein2011@yahoo.com>
2020-07-08 22:41:13 +0430

    new games programms

--- coding/Python/Daily_code.py
+++ coding/Python/Daily_code.py
@@ -132,4 +132,18 @@ def Poki_game(l):
 
 Poki_game([1,5,6,8,9])
 
+import random
+def Memory_Match(num_games=5):
+    ls= random.sample(range(0,100), num_games)
+    score=0
+    
+    for i in range(num_games):
+        init=random.choice(ls)
+        print(init)
+        last_num=int(input('what was the last number? '))
+        if last_num == init:
+            score+=1
+    print('your score is {} out of {}'.format(score,num_games)) 
+
+Memory_Match(5)
 

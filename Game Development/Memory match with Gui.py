b02642bcaeb2c5d127645f440f3c65bda5c9c7bc
h0ssein2011 <h0ssein2011@yahoo.com>
2020-07-09 20:02:04 +0430

    add basic Tkinter codes

--- Game Development/Memory match with Gui.py
+++ Game Development/Memory match with Gui.py
@@ -3,8 +3,16 @@ m = tk.Tk()
 
 m.title('Counting Seconds')
 
-button = tk.Button(m,text='stop',width = 25 , command=m.destroy)
-button.pack()
-m.mainloop()
+# label = tk.Label(text='what is your name? ',foreground='red',background='#FF00FF')
+txt =  tk.Entry(text='name', width=24)
+txt.insert(0,'Just do it')
+button = tk.Button(m,text='Finish',width = 25 , command=m.destroy)
 
+name = txt.get()
+print(name)
+# label.pack()
+button.pack()
+txt.pack()
+
+m.mainloop()
 

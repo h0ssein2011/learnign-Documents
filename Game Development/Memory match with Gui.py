eaca25011a7df067f997c8373e7057174f3a02f9
h0ssein2011 <h0ssein2011@yahoo.com>
2020-07-10 01:37:58 +0430

    build a simple temprature covertor

--- Game Development/Memory match with Gui.py
+++ Game Development/Memory match with Gui.py
@@ -6,13 +6,67 @@ m.title('Counting Seconds')
 # label = tk.Label(text='what is your name? ',foreground='red',background='#FF00FF')
 txt =  tk.Entry(text='name', width=24)
 txt.insert(0,'Just do it')
+txt.pack()
 button = tk.Button(m,text='Finish',width = 25 , command=m.destroy)
 
 name = txt.get()
 print(name)
 # label.pack()
 button.pack()
-txt.pack()
+
 
 m.mainloop()
+
+
+
+import tkinter as tk
+
+window = tk.Tk()
+
+for i in range(3):
+    for j in range(3):
+        frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j)
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+        label.pack()
+
+window.mainloop()
+
+
+
+
+# simple temprature calculator
+import tkinter as tk
+def faranhite_to_celcious():
+    faranhite = ent_temoarture.get()
+    celcious = (5/9) * (float(faranhite) - 32)
+    lbl_result['text'] = f"{round(celcious, 2)} \N{DEGREE CELSIUS}"
+
+
+window = tk.Tk()
+
+window.title('simple temprature calculator')
+frm_entry = tk.Frame(master = window)
+ent_temoarture = tk.Entry(master=frm_entry , width=10)
+lbm_label = tk.Label(master = frm_entry , text="\N{DEGREE FAHRENHEIT}")
+
+
+ent_temoarture.grid(row = 0 , column=0 ,sticky='e')
+lbm_label.grid(row = 0 , column=1 ,sticky='w')
+
+btn_convert =tk.Button(master = window , 
+                       text="\N{RIGHTWARDS BLACK ARROW}",
+                       command=faranhite_to_celcious)
+lbl_result = tk.Label(master=window , text="\N{DEGREE CELSIUS}")
+
+frm_entry.grid(row=0,column=0 ,padx=100)
+btn_convert.grid(row=0,column=1 ,pady=10)
+lbl_result.grid(row=0,column=2 ,padx=10)
+
+window.mainloop()
+
 

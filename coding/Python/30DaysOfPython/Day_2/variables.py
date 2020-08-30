90dd316a634f6bc4170f4d2218b457cabfafcd52
h0ssein2011 <h0ssein2011@yahoo.com>
2020-08-30 23:36:55 +0430

    add day 2 practice

---
+++ coding/Python/30DaysOfPython/Day_2/variables.py
@@ -0,0 +1,64 @@
+first_name='Hossein'
+last_name='Mortazavi'
+full_name = first_name + ' ' + last_name
+country = 'iran'
+city ='Tehran'
+age = 35
+year = 2020
+is_married = False
+is_true = False
+is_light =True
+name, family,age,country,city,year = 'Ali','Rezaei',32,'Iran','Tehran',2019
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+
+print(len(first_name))
+print(len(first_name) == len(last_name))
+
+
+
+num_one =5 
+num_two =4
+_total = num_one + num_two 
+_diff = num_one - num_two
+__product = num_one * num_two
+_division = num_one / num_two
+_remainder = num_one // num_two
+_exp = num_one ** num_two
+_floor_division = round(num_one / num_two,0)
+
+print(_total)
+print(_diff)
+print(__product)
+print(_division)
+print(_remainder)
+print(_exp)
+print(_floor_division)
+
+
+
+radius = 30 
+pi= 3.14
+area_of_circle = radius * radius **2
+circum_of_circle = 2 * radius * pi
+print(area_of_circle)
+print(circum_of_circle)
+
+radius = int(input())
+area_of_circle = radius * radius **2
+print(area_of_circle)
+
+
+
+
+
+
+
+
+
+

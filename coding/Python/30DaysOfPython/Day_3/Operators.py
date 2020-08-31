b4bfcd91d1d2c08667bd239694f4715666c42ccb
h0ssein2011 <h0ssein2011@yahoo.com>
2020-09-01 00:48:42 +0430

    operators codes

---
+++ coding/Python/30DaysOfPython/Day_3/Operators.py
@@ -0,0 +1,51 @@
+age = 35
+height = 1.68
+complex = 1 + 2j
+
+a = input('Please Enter an integer number as base triangle: ')
+h = input('Please Enter an integer number as  height triangle: ')
+print('the area is:' , int(a) * int(h) * 0.5)
+
+sid_a = int(input('Please Enter an integer number as side a triangle: '))
+sid_b = int(input('Please Enter an integer number as side b triangle: '))
+sid_c = int(input('Please Enter an integer number as side c triangle: '))
+
+print('The perimeter is: ',sid_a+sid_b +sid_c)
+
+width = int(input('please input rectangle with: '))
+length = int(input('please input rectangle length: '))
+print(' the area is: ',width *length)
+
+radius = int(input(prompt='please input the radius: '))
+print(radius ** 2 * 3.14)
+
+X= -3
+y=X^2 + 6*X + 9 
+print(y)
+
+
+print(len('python') != len('jargon'))
+print('on'  in 'python' )
+print('on'  in 'jargon' )
+print(str(float(len('python'))))
+
+print( 27 % 2 == 0)
+print(7 // 3  == int(2.7))
+print(type('10')== type(10))
+print( int(9.8) == 10)
+
+h=int(input('Please enter the number of hours:'))
+rate=int(input('Please enter of hourly rate:'))
+print(h * rate)
+
+years = int(input())
+print( 'you lived for:', years * 365 * 24 * 60 * 60,'seconds')
+
+for i in range(5):
+    for j in range(5):
+        if i == 0 or j==1:
+            print(1)
+        else :
+            print(i*j)
+
+        

c3b339c343b4caa147b054ff54122cf82a7ade49
Hossein Mortazavi <h0ssein2011@yahoo.com>
2021-08-18 17:36:58 +0300

    edit code

--- coding/Python/Slot Games/Seven 7s.py
+++ coding/Python/Slot Games/Seven 7s.py
@@ -3,41 +3,43 @@ credits = 20
 bet_amount = 0.20
 shapes = ['7','Bar','7-Bar','Bar-7']
 win = 0
-spins = 100
+win_lim = 100
+chance = ['7']
 
-while credits > 0:
-    chance = ['7']
-    for i in range(spins):
-        credits -= bet_amount
+while credits > 0 and credits <= win_lim:
+    credits -= bet_amount
+    chance.append(random.choice(shapes))
+    if chance.count('7') == 2 :
+        credits += 0.40
         chance.append(random.choice(shapes))
-        print(i, chance)
-        if chance.count('7') == 2 :
-            credits += 0.40
-            chance.append(random.choice(shapes))
 
-            if chance.count('7') == 3 :
-                credits += 2.0
+        if chance.count('7') == 3 :
+            credits += 2.0
+            chance.append(random.choice(shapes))
+        else :
+            chance = ['7']
+            pass
+
+            if chance.count('7') == 4 :
+                credits += 5
                 chance.append(random.choice(shapes))
             else :
                 chance = ['7']
+                pass
 
-                if chance.count('7') == 4 :
-                    credits += 5
+                if chance.count('7') == 5 :
+                    credits += 15 
                     chance.append(random.choice(shapes))
                 else :
                     chance = ['7']
+                    pass
 
-                    if chance.count('7') == 5 :
-                        credits += 15 
-                        chance.append(random.choice(shapes))
-                    else :
+                    if chance.count('7') == 6 :
+                        credits += 50
                         chance = ['7']
-
-                        if chance.count('7') == 6 :
-                            credits += 50
-                            chance = ['7']
-        else :
-            chance = ['7']
+                        pass
+    else :
+        chance = ['7']
     print('your final credit: ',credits)
         
         

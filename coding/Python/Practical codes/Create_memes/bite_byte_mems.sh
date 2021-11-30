8c0204045deeb64c7e4a20e6bcd9c736142844f7
hossein.mortazavi <hossein.mortazavi@oc.eu>
2021-11-30 17:07:49 +0200

    create mems with bit byte from jadi

--- coding/Python/Practical codes/Create_memes/bite_byte_mems.sh
+++ coding/Python/Practical codes/Create_memes/bite_byte_mems.sh
@@ -2,6 +2,11 @@
 
 word="rabbit"
 
-img_url="$./dl_photo_from_google.py $word" 
-wget $image_url -O word.png
-convert word.png -resize 200x200 word.png
+img_url=`./dl_photo_from_google.py $word` 
+# echo "Image URL: $img_url"
+wget $img_url -O tmp.png
+convert tmp.png -resize 200x200 tmp.png
+magick tmp.png -tile 1x1  -title $word top.png
+magick tmp.png -tile 4x2  -title $word tmp.png tmp.png tmp.png tmp.png tmp.png tmp.png tmp.png tmp.png bottom.png
+
+

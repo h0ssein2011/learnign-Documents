40e4a67bb3c3034c706fbe73acfdfcecfcab4326
hossein.mortazavi <hossein.mortazavi@oc.eu>
2022-04-21 10:04:04 +0300

    pull data

--- coding/Python/flag.py
+++ coding/Python/flag.py
@@ -3,7 +3,23 @@ from PIL import Image
 img = Image.open('flag.png')
 img = img.convert('RGB')
 print(img.size)
-r, g, b = img.getpixel((1, 1))
-print(r, g, b)
+
+for i in range(img.size[0]):
+        r, g, b = img.getpixel((i, 1))
+        print( r,g,b)
 
 
+
+for i in range(int(img.size[0]/3)):
+        r, g, b = img.getpixel((i, 1))
+        print( chr(b),end=' ')
+
+for i in range(int(img.size[0]/3),int(img.size[0]*2/3)):
+        r, g, b = img.getpixel((i, 1))
+        print( chr(255),end=' ')
+        
+
+r,g,b =84 ,109, 140
+print(chr(r),chr(g),chr(b),end=' ')
+r,g,b =222 ,96 ,110
+print(chr(r),chr(g),chr(b),end=' ')

d25059fa89ac3e6dea6eee481573bf8b838db1cc
Hossein Mortazavi <Hossein.Mortazavi@oc.eu>
2022-05-16 16:56:03 +0300

    add case stduy 1 problem 6&7

--- coding/Sql/8 Week SQL Challenge/w1.sql
+++ coding/Sql/8 Week SQL Challenge/w1.sql
@@ -52,7 +52,50 @@ select customer_id ,product_id ,count(*) as pt
 
 
 -- 6. Which item was purchased first by the customer after they became a member?
+with
+tr_status as
+(SELECT s.*, 
+  case when s.order_date > m.join_date or m.join_date is null  then 'after_join' else 'before_join' end as transactions 
+ -- ,case when m.join_date is null then s.order_date else m.join_date end as join_date
+FROM dannys_diner.sales s
+ left join dannys_diner.members m
+ ON s.customer_id = m.customer_id
+  )
+ , Min_Purchase as 
+  (select customer_id ,product_id , min(order_date)
+  from tr_status
+  where transactions = 'after_join'
+  group by 1,2)
+  
+  select distinct customer_id ,mn.product_name
+  from Min_Purchase mp
+  join dannys_diner.menu mn
+  ON mn.product_id = mp.product_id
+  order by 1
+
 -- 7. Which item was purchased just before the customer became a member?
+with
+tr_status as
+(SELECT s.*, 
+  case when s.order_date > m.join_date or m.join_date is null  then 'after_join' else 'before_join' end as transactions 
+ -- ,case when m.join_date is null then s.order_date else m.join_date end as join_date
+FROM dannys_diner.sales s
+ left join dannys_diner.members m
+ ON s.customer_id = m.customer_id
+  )
+ , Min_Purchase as 
+  (select customer_id ,product_id , min(order_date)
+  from tr_status
+  where transactions = 'before_join'
+  group by 1,2)
+  
+  select distinct customer_id ,mn.product_name
+  from Min_Purchase mp
+  join dannys_diner.menu mn
+  ON mn.product_id = mp.product_id
+  order by 1
+
+
 -- 8. What is the total items and amount spent for each member before they became a member?
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?

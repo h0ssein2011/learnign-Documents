df1e87f07963e1d3174812877f155fb38ef4e314
h0ssein2011 <h0ssein2011@yahoo.com>
2022-05-17 17:21:29 +0300

    Finish Case Study 1

--- coding/Sql/8 Week SQL Challenge/w1.sql
+++ coding/Sql/8 Week SQL Challenge/w1.sql
@@ -53,49 +53,79 @@ select customer_id ,product_id ,count(*) as pt
 
 -- 6. Which item was purchased first by the customer after they became a member?
 with
-tr_status as
-(SELECT s.*, 
-  case when s.order_date > m.join_date or m.join_date is null  then 'after_join' else 'before_join' end as transactions 
- -- ,case when m.join_date is null then s.order_date else m.join_date end as join_date
+sales_CTE as
+(SELECT s.*, mn.product_name ,dense_rank() over( PARTITION by s.customer_id order by order_date ASC ) as ranks
 FROM dannys_diner.sales s
- left join dannys_diner.members m
+ join dannys_diner.members m
  ON s.customer_id = m.customer_id
+ join dannys_diner.menu mn
+ ON mn.product_id = s.product_id
+ where s.order_date > m.join_date
   )
- , Min_Purchase as 
-  (select customer_id ,product_id , min(order_date)
-  from tr_status
-  where transactions = 'after_join'
-  group by 1,2)
-  
-  select distinct customer_id ,mn.product_name
-  from Min_Purchase mp
-  join dannys_diner.menu mn
-  ON mn.product_id = mp.product_id
-  order by 1
+  select customer_id ,product_name,order_date
+  from sales_CTE
+  where ranks =1
 
 -- 7. Which item was purchased just before the customer became a member?
 with
-tr_status as
-(SELECT s.*, 
-  case when s.order_date > m.join_date or m.join_date is null  then 'after_join' else 'before_join' end as transactions 
- -- ,case when m.join_date is null then s.order_date else m.join_date end as join_date
+sales_CTE as
+(SELECT s.*, mn.product_name ,dense_rank() over( PARTITION by s.customer_id order by order_date ASC ) as ranks
 FROM dannys_diner.sales s
- left join dannys_diner.members m
+ join dannys_diner.members m
  ON s.customer_id = m.customer_id
+ join dannys_diner.menu mn
+ ON mn.product_id = s.product_id
+ where s.order_date < m.join_date
   )
- , Min_Purchase as 
-  (select customer_id ,product_id , min(order_date)
-  from tr_status
-  where transactions = 'before_join'
-  group by 1,2)
-  
-  select distinct customer_id ,mn.product_name
-  from Min_Purchase mp
-  join dannys_diner.menu mn
-  ON mn.product_id = mp.product_id
-  order by 1
+  select customer_id ,product_name,order_date
+  from sales_CTE
+  where ranks =1
 
 
 -- 8. What is the total items and amount spent for each member before they became a member?
+with
+sales_CTE as
+(SELECT s.*,mn.price
+FROM dannys_diner.sales s
+ join dannys_diner.members m
+ ON s.customer_id = m.customer_id
+ join dannys_diner.menu mn
+ ON mn.product_id = s.product_id
+ where s.order_date < m.join_date
+  )
+  select  customer_id,count(*) as total_items ,sum(price) as spent_amount 
+  from sales_CTE s  
+  group by 1
+
 -- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+with Sales_CTE as 
+(SELECT s.*,mn.price ,
+case when s.product_id=1 then mn.price * 10 * 2 else  mn.price * 10 end as points
+FROM dannys_diner.sales s
+ join dannys_diner.menu mn
+ ON mn.product_id = s.product_id)
+ 
+ select customer_id ,sum(points) as points
+ from Sales_CTE
+ group by 1
+ order by 1
+
 -- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+with Sales_CTE as 
+(SELECT s.*,mn.price ,
+case when s.product_id=1 then mn.price * 10 * 2 
+ when s.order_date >= m.join_date and  s.order_date < m.join_date + 7 
+ 							then mn.price * 10 * 2 else  mn.price * 10 end as points
+FROM dannys_diner.sales s
+ JOIN dannys_diner.members m
+ ON m.customer_id = s.customer_id
+ join dannys_diner.menu mn
+ ON mn.product_id = s.product_id
+where EXTRACT(MONTH FROM s.order_date::DATE) = 1
+)
+ 
+ select customer_id ,sum(points) as points
+ from Sales_CTE
+ 
+ group by 1
+ order by 1

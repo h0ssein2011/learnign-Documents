823c0431dfdd2650f6d93c2bfdabddd47e4e5244
h0ssein2011 <h0ssein2011@yahoo.com>
2022-05-20 17:48:33 +0300

    answer question 3 & 4

--- coding/Sql/8_Week_sql_challanges/w2.sql
+++ coding/Sql/8_Week_sql_challanges/w2.sql
@@ -14,10 +14,24 @@ with Clean_runner_orders_CTE as
 (select order_id,runner_id,pickup_time, 
   translate(distance,'km,null','') as distance,
   translate(duration,'minutes,mins,null','') as duration,
-  replace(cancellation,'null','') as cancellation1
- ,cancellation
+ case when cancellation in ('','null') then NULL else cancellation end as cancellation
 
 FROM pizza_runner.runner_orders
  )
  
- select * from Clean_runner_orders_CTE
+ select runner_id ,count(distinct order_id )
+ from Clean_runner_orders_CTE
+ where cancellation is NULL
+ group by 1
+
+
+-- How many of each type of pizza was delivered?
+-- # order id 3 was contain two type of pizza so number of delivery is 2 times
+select pn.pizza_name , count(distinct c.order_id )
+FROM pizza_runner.customer_orders c
+join pizza_runner.runner_orders r
+ON r.order_id = c.order_id
+join pizza_runner.pizza_names pn
+ON c.pizza_id = pn.pizza_id
+where r.pickup_time IS NOT NULL
+group by 1

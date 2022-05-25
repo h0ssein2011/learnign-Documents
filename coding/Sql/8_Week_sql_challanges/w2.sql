75bf1c009f685e0a1f74e789b5e631fc8a211cc5
h0ssein2011 <h0ssein2011@yahoo.com>
2022-05-25 17:51:33 +0300

    Add answer to question 7 w2

--- coding/Sql/8_Week_sql_challanges/w2.sql
+++ coding/Sql/8_Week_sql_challanges/w2.sql
@@ -9,7 +9,7 @@ select count(distinct customer_id) as uq_customers
 FROM pizza_runner.customer_orders
 
 
--- How many successful orders were delivered by each runner?
+-- 3How many successful orders were delivered by each runner?
 with Clean_runner_orders_CTE as 
 (select order_id,runner_id,pickup_time, 
   translate(distance,'km,null','') as distance,
@@ -25,7 +25,7 @@ FROM pizza_runner.runner_orders
  group by 1
 
 
--- How many of each type of pizza was delivered?
+-- 4How many of each type of pizza was delivered?
 -- # order id 3 was contain two type of pizza so number of delivery is 2 times
 select pn.pizza_name , count(distinct c.order_id )
 FROM pizza_runner.customer_orders c
@@ -35,3 +35,37 @@ join pizza_runner.pizza_names pn
 ON c.pizza_id = pn.pizza_id
 where r.pickup_time IS NOT NULL
 group by 1
+
+--5- How many Vegetarian and Meatlovers were ordered by each customer?
+select c.customer_id ,pn.pizza_name , count(distinct c.order_id )
+FROM pizza_runner.customer_orders c
+join pizza_runner.pizza_names pn
+ON c.pizza_id = pn.pizza_id
+group by 1 ,2
+
+--6-What was the maximum number of pizzas delivered in a single order?
+select t.count_pizas from
+(
+select c.order_id , count(distinct c.pizza_id ) as count_pizas
+FROM pizza_runner.customer_orders c
+group by 1
+order by 2 Desc
+) t
+limit 1
+
+--7 For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+WITH cleand_orders as 
+(select * , 
+case when  exclusions in ('null',NULL,'')  then NULL else exclusions end exclusions_cleaned,
+case when  extras in ('null',NULL,'')  then NULL else extras end extras_cleaned
+
+from pizza_runner.customer_orders)
+select customer_id ,sum( case when (exclusions_cleaned is NULL) and (extras_cleaned is NULL)    
+                   			then 0 else 1 end) as with_change,
+                   sum( case when (exclusions_cleaned is NULL) and (extras_cleaned is NULL)    
+                   			then 1 else 0 end) as with_out_change
+                   
+              
+from cleand_orders
+group by 1
+order by 1

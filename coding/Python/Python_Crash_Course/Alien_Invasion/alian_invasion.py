c14ff4d30e0ccb814524efdc9bc5bc1ed0d9c603
h0ssein2011 <h0ssein2011@yahoo.com>
2022-11-11 17:46:20 +0200

    Add event module

--- coding/Python/Python_Crash_Course/Alien_Invasion/alian_invasion.py
+++ coding/Python/Python_Crash_Course/Alien_Invasion/alian_invasion.py
@@ -16,15 +16,22 @@ class Alien_invasion:
     
     def run_game(self):
         while True:
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_screen()
+            
+
+    def _check_events(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                     print('game has finished')
-            # redraw the screeen during pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _update_screen(self):
+        # redraw the screeen during pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+        
 
 if __name__ == '__main__':
     ai = Alien_invasion()

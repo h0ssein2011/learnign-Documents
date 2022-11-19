d53bb3ee47dddc1aa3ef7493886f0b856f666152
h0ssein2011 <h0ssein2011@yahoo.com>
2022-11-19 21:53:45 +0200

    define project's steps

--- coding/Python/Python_Crash_Course/Alien_Invasion/alian_invasion.py
+++ coding/Python/Python_Crash_Course/Alien_Invasion/alian_invasion.py
@@ -1,3 +1,31 @@
+"""
+In Alien Invasion, the player controls a rocket ship that appears
+at the bottom center of the screen. The player can move the ship
+right and left using the arrow keys and shoot bullets using the
+spacebar. When the game begins, a fleet of aliens fills the sky
+and moves across and down the screen. The player shoots and
+destroys the aliens. If the player shoots all the aliens, a new fleet
+appears that moves faster than the previous fleet. If any alien hits
+the player’s ship or reaches the bottom of the screen, the player
+loses a ship. If the player loses three ships, the game ends.
+
+required objects:
+ - screen
+ - Rocket ship
+ - fleet of aliens
+
+Modules:
+- move Rocket ship(left/right)
+- shoots 
+- increase speed
+- run game
+
+Rules :
+- If the player shoots all the aliens, a new fleet appears that moves faster than the previous fleet
+- If any alien hits the player’s ship or reaches the bottom of the screen, the player loses a ship. 
+- If the player loses three ships, the game ends.
+"""
+
 import sys
 import pygame
 from setting import Settings

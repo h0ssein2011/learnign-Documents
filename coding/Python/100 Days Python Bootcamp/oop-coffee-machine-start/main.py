ecbbe3ca4d863539d3e53518c2f2c7ee24687467
h0ssein2011 <h0ssein2011@yahoo.com>
2022-11-24 17:20:41 +0200

    Add User order Prompt

--- coding/Python/100 Days Python Bootcamp/oop-coffee-machine-start/main.py
+++ coding/Python/100 Days Python Bootcamp/oop-coffee-machine-start/main.py
@@ -1,3 +1,14 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+def get_order(Options = ['espresso','latte','cappuccino']):
+    order = input('what would you like?').lower()
+    if order not in Options:
+        NameError('Please enter a valid order')
+    else :
+        return order  
+
+if __name__== 'main' :
+    get_order = get_order()
+    get_order()

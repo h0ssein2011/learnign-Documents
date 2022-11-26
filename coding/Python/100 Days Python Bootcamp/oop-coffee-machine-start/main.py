7e97040215f887246fbf26e06dd5f7133878c956
h0ssein2011 <h0ssein2011@yahoo.com>
2022-11-26 18:20:34 +0200

    Add report to returns resources

--- coding/Python/100 Days Python Bootcamp/oop-coffee-machine-start/main.py
+++ coding/Python/100 Days Python Bootcamp/oop-coffee-machine-start/main.py
@@ -2,12 +2,13 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-def get_order(Options = ['espresso','latte','cappuccino']):
+def get_order(Coffe_machine= CoffeeMaker(),Options = ['espresso','latte','cappuccino']):
     order = input('what would you like?').lower()
     if order not in Options:
         print('Please enter a valid order')
+
     else :
-        return order  
+        return Coffe_machine.report()  
 
 if __name__ == "__main__" :
     # get_order = get_order()

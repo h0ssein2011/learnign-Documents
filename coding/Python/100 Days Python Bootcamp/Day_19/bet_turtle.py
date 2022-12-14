cc9d24c0a9a98a419bc1a0e045387f9f12367dad
hossein <h0ssein2011@yahoo.com>
2022-12-14 17:42:42 +0200

    complete bet turtle

--- coding/Python/100 Days Python Bootcamp/Day_19/bet_turtle.py
+++ coding/Python/100 Days Python Bootcamp/Day_19/bet_turtle.py
@@ -3,23 +3,42 @@ import random
 
 
 screen = Screen()
-screen.setup(width=800, height=800)
+screen.setup(width=1000, height=800)
 screen.colormode(255)
 
 y_positions =[-70,-40,-10,20,50,80,110]
 
-def random_color():
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    color = (r,g,b)
-    return color
+colors =['yellow', 'green', 'orange', 'black','red','blue','purple']
+bet_turtle = screen.textinput('User data',f'Which colors do you think will win? {colors}')
 
 
+Turtles = []
 for i in range(len(y_positions)):
     new_turtle = Turtle(shape="turtle")
     new_turtle.penup()
-    new_turtle.goto(x=-380 , y=y_positions[i])
-    new_turtle.color(random_color())
+    new_turtle.goto(x=-450 , y=y_positions[i])
+    new_turtle.color(colors[i])
+    Turtles.append(new_turtle)
 
+win_pos_x = 400
+games_running = True
+
+while games_running:
+    idx = 0
+    for turtle in Turtles:
+        mv_amount = random.randint(1,10)
+        turtle.forward(mv_amount)
+        if turtle.pos()[0] >= win_pos_x:
+            games_running = False
+            # print(colors[idx],'wins')
+            win_color = colors[idx]
+        
+        
+        idx += 1
+if bet_turtle == win_color:
+    print('Great you predict it correctly')
+
+else :
+    print(f'Sorry you said {bet_turtle} will win but {win_color} won')
+            
 screen.exitonclick()

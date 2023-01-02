22b4400a77686f3f255cf250bdf3206573074a71
h0ssein2011 <h0ssein2011@yahoo.com>
2023-01-02 18:40:06 +0200

    Add Crossing capstone project

---
+++ coding/Python/100 Days Python Bootcamp/Day_23/car_manager.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+import random
+
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=1, stretch_len=3)
+        self.color(random.choice(COLORS))
+        self.penup()

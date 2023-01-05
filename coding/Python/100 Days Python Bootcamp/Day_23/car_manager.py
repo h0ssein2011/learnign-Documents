91295ec927e05cf08a44f3e4b454a589a5d4f11b
h0ssein2011 <h0ssein2011@yahoo.com>
2023-01-05 17:55:09 +0200

    Debug car manager code

--- coding/Python/100 Days Python Bootcamp/Day_23/car_manager.py
+++ coding/Python/100 Days Python Bootcamp/Day_23/car_manager.py
@@ -1,27 +1,31 @@
 from turtle import Turtle
 import random
 
-
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-
-
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("square")
-        self.shapesize(stretch_wid=1, stretch_len=3)
-        self.color(random.choice(COLORS))
-        self.penup()
-        self.setheading(-180)
-        self.x = random.randint(280,300)
-        self.y = random.randint(-300,300)
-        self.goto(self.x, self.y)
-        self.move_speed = 0.1
+        self.car_list = []
         
 
     def move_car(self):
-        self.forward(STARTING_MOVE_DISTANCE)
+        for car in self.car_list:
+            car.forward(STARTING_MOVE_DISTANCE)
+    
+    def create_car(self):
+        rand_number = random.randint(1,6)
+        if rand_number == 1:
+            self.shape("square")
+            self.shapesize(stretch_wid=1, stretch_len=3)
+            self.color(random.choice(COLORS))
+            self.penup()
+            self.setheading(-180)
+            self.y = random.randint(-280,280)
+            self.goto(300, self.y)
+            self.car_list.append(self)
+            print(self.car_list)
+        
         
--- coding/Python/100 Days Python Bootcamp/Day_23/main.py
+++ coding/Python/100 Days Python Bootcamp/Day_23/main.py
@@ -16,36 +16,26 @@ screen.listen()
 screen.onkey(player.move_forward, "Up")
 
 
-
-wait_time = 0
-car_list = []
-car_list.append(CarManager())
-car_list.append(CarManager())
+car_manager = CarManager()
 
 
 game_is_on = True
 while game_is_on:
-    time.sleep(car_list[0].move_speed)
+    time.sleep(0.1)
     screen.update()
-    wait_time += 0.1
-    #every 1 seconds a new car is added to the screen
-    if wait_time > 3:
-        wait_time = 0
-        new_car = CarManager()
-        car_list.append(new_car)
-    # move a random car in every 0.1 seconds
-    ready_car =random.randint(1, len(car_list)-1)
-    car_list[ready_car].move_car()
-
+    
+    car_manager.create_car()
+    car_manager.move_car()
+    
     # check if Player reached the top screen
     if player.ycor() > 280 :
         score_board.update_score_baord()
         score_board.show_score()
         player.reset_player()
-        car_list[0].move_speed *= 0.9
+        # car_manager.car_list[0].move_speed *= 0.9
     
     # detect collision with car
-    for car in car_list:
+    for car in car_manager.car_list:
         if car.distance(player) < 20:
             game_is_on = False
             score_board.game_over()

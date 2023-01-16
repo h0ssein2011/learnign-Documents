66a0b70bea4454417b9859f854c1dc27d3d311b3
hossein <h0ssein2011@yahoo.com>
2023-01-16 18:57:19 +0200

    Add high score to the scoreboard and reset snake

--- coding/Python/100 Days Python Bootcamp/Day_24/food.py
+++ coding/Python/100 Days Python Bootcamp/Day_24/food.py
@@ -1,19 +1,19 @@
-import random
 from turtle import Turtle
+import random
+
 
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
         self.shape("circle")
-
-        self.color("blue")
         self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
         self.refresh()
-    
+
     def refresh(self):
-        random_x_loc = random.randint(-280,280)
-        random_y_loc = random.randint(-280,280)
-        self.goto(random_x_loc,random_y_loc)
- 
-          
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
--- coding/Python/100 Days Python Bootcamp/Day_24/main.py
+++ coding/Python/100 Days Python Bootcamp/Day_24/main.py
@@ -1,37 +1,51 @@
-import time
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.setup(width=800, height=600)
-screen.title("Snake Game")
+screen.title("My Snake Game")
+screen.tracer(0)
 
 snake = Snake()
-Food = Food()
+food = Food()
+scoreboard = Scoreboard()
 
-screen.tracer(0)
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(snake.move_speed)
-    
-    # detect collisions with wall
-    if snake.xcor() > 290 or snake.xcor() < -290 or snake.ycor() > 290 or snake.ycor() < -290:
-        game_is_on = False
+    time.sleep(0.1)
+    snake.move()
 
-    # detect collision with snake
-    if snake.distance(Food) < 15:
-        # game_is_on = False
-        print('Collision with Food')
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+        
+
 
 
 

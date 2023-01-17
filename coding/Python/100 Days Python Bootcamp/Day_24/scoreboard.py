d4304fac2032adb999e58219dc3e4addc40fe4b8
h0ssein2011 <h0ssein2011@yahoo.com>
2023-01-17 18:38:37 +0200

    Add data file to store high score

--- coding/Python/100 Days Python Bootcamp/Day_24/scoreboard.py
+++ coding/Python/100 Days Python Bootcamp/Day_24/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt', 'r') as f:
+                self.high_score = int(f.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,9 +23,10 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('data.txt', 'w') as f:
+                f.write(f'{self.high_score}')
         self.score = 0
         self.update_scoreboard()
-
 
     def increase_score(self):
         self.score += 1

c4c66569f44216422e223e0eadbe5d433cc8b718
h0ssein2011 <h0ssein2011@yahoo.com>
2023-01-20 18:12:07 +0200

    Finish the merge mail challange

--- coding/Python/100 Days Python Bootcamp/Day_24/Mail_Merge/main.py
+++ coding/Python/100 Days Python Bootcamp/Day_24/Mail_Merge/main.py
@@ -8,14 +8,14 @@
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 with open('Input/Letters/starting_letter.txt','r') as f:
-    raw_text = f.readlines()
+    raw_text = f.read()
 
 with open('Input/Names/invited_names.txt','r') as f:
     invited_names = f.readlines()
 
 
 for name in invited_names:
-    for text in raw_text:
-        text = text.replace('[name]',name.strip())
-        with open(f'Output/{name}.txt','a') as f:
-            f.write(text + "\n")
+    
+    raw_text = raw_text.replace('[name]',name.strip())
+    with open(f'Output/leffer_for_{name}.txt','w') as f:
+        f.write(raw_text + "\n")

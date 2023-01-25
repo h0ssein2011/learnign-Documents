f279b3669bd15eb6f27ca91a0ad8c2c6ba7b2bdb
h0ssein2011 <h0ssein2011@yahoo.com>
2023-01-25 18:32:25 +0200

    Edit us states challenge

--- coding/Python/100 Days Python Bootcamp/Day_25/main.py
+++ coding/Python/100 Days Python Bootcamp/Day_25/main.py
@@ -1,7 +1,22 @@
-from turtle import Turtle , Screen
+import turtle 
+import pandas as pd
 
-screen = Screen()
-screen.bgpic('./blank_states_img.gif')
+screen = turtle.Screen()
+screen.title('U.S States Game')
+img = "blank_states_img.gif"
+screen.addshape(img)
 
+turtle.shape(img)
+
+us_states = pd.read_csv('./50_states.csv')
+
+
+for i in range(len(us_states)):
+    answer = screen.textinput(title="Guess the state" , prompt="what is the another state name?")
+    answer = answer.lower().strip()
+    df_temp = us_states.query('state == @answer')
+    if len(df_temp) ==1 :
+        print('x is :',df_temp.x)
+        print('y is :',df_temp.y)
 
 screen.exitonclick()

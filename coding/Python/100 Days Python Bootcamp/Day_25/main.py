5b3d6eb9a636c87d62f6c274d0c7c254372152b0
h0ssein2011 <h0ssein2011@yahoo.com>
2023-01-26 18:07:53 +0200

    complete challenge

--- coding/Python/100 Days Python Bootcamp/Day_25/main.py
+++ coding/Python/100 Days Python Bootcamp/Day_25/main.py
@@ -1,5 +1,14 @@
+"""
+Todo:
+- add gif file to the background
+- prompt for each states a question and find the x and y coordinates from the csv file
+- send the prompted text to the cordination
+"""
+
+
 import turtle 
 import pandas as pd
+
 
 screen = turtle.Screen()
 screen.title('U.S States Game')
@@ -9,14 +18,30 @@ screen.addshape(img)
 turtle.shape(img)
 
 us_states = pd.read_csv('./50_states.csv')
-
+right_answers =[]
 
 for i in range(len(us_states)):
-    answer = screen.textinput(title="Guess the state" , prompt="what is the another state name?")
+    answer = screen.textinput(title=f"Guess the state {i+1}/50 " , prompt="what is the another state name?")
     answer = answer.lower().strip()
-    df_temp = us_states.query('state == @answer')
-    if len(df_temp) ==1 :
-        print('x is :',df_temp.x)
-        print('y is :',df_temp.y)
+    state_list = us_states['state'].str.lower().tolist()
+    if answer in right_answers:
+        print('You said it before')
+    elif answer in state_list:
+        idx = state_list.index(answer) 
+        x_cor = us_states.loc[idx,'x']
+        y_cor = us_states.loc[idx,'y']
+        right_answers.append(answer)
+        # create a text to move to x and y coordinates
+        text_turtle = turtle.Turtle()
+        text_turtle.hideturtle()
+        text_turtle.shape(name=None)
+        text_turtle.penup()
+        text_turtle.goto(x_cor,y_cor)
+        text_turtle.write(answer)
+        # text_turtle.hideturtle()
+    else :
+        print(f'{answer} is not a U.S states')
+        pass
+
 
 screen.exitonclick()

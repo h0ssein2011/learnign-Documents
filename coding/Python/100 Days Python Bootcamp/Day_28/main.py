e248a7dea3df82823ae6ee358473ad8e5f3f277a
h0ssein2011 <h0ssein2011@yahoo.com>
2023-02-20 18:06:10 +0200

    Add Pomodoro app with TKinter

---
+++ coding/Python/100 Days Python Bootcamp/Day_28/main.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title('Pomodoro')
+window.config(padx=100, pady=53,bg=YELLOW)
+
+#add image
+canvas = Canvas(width=200, height=225,bg=YELLOW,highlightthickness=0)
+tomato_img = PhotoImage(file="./tomato.png")
+canvas.create_image(100,112,image=tomato_img )
+canvas.create_text(103,130 , text = "00:00",fill='white' ,font=(FONT_NAME , 35 ,'bold'))
+canvas.pack()
+
+
+
+window.mainloop()

93435b4865bf7aeaaf78b3c53196a306487994ef
h0ssein2011 <h0ssein2011@yahoo.com>
2023-02-22 17:35:05 +0200

    Add buttons to the Pomodoro app

--- coding/Python/100 Days Python Bootcamp/Day_28/main.py
+++ coding/Python/100 Days Python Bootcamp/Day_28/main.py
@@ -9,6 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+TICK = '✓'
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -20,14 +21,25 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title('Pomodoro')
-window.config(padx=100, pady=53,bg=YELLOW)
+window.config(padx=100, pady=20,bg=YELLOW)
+
 
 #add image
-canvas = Canvas(width=200, height=225,bg=YELLOW,highlightthickness=0)
+canvas = Canvas(width=200, height=255,bg=YELLOW,highlightthickness=0)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas.create_image(100,112,image=tomato_img )
 canvas.create_text(103,130 , text = "00:00",fill='white' ,font=(FONT_NAME , 35 ,'bold'))
+canvas.create_text(103,20 , text = "Timer",fill=GREEN ,font=(FONT_NAME , 35 ,'bold'))
+canvas.create_text(103,280 , text = TICK,fill=GREEN ,font=(FONT_NAME , 20 ,'bold'))
+
+
 canvas.pack()
+
+start = Button(window, text = "Start",bg='white')
+finish = Button(window, text = "Finish",bg='white')
+start.pack(side='left')
+finish.pack(side='right')
+
 
 
 

5c69bc5ce47b4f273d911b53efd1dcbc7559df3d
h0ssein2011 <h0ssein2011@yahoo.com>
2023-03-06 18:21:07 +0200

    Add timer to the pomodoro app

--- coding/Python/100 Days Python Bootcamp/Day_28/main.py
+++ coding/Python/100 Days Python Bootcamp/Day_28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -14,14 +15,21 @@ TICK = '✓'
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(WORK_MIN * 60 )
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 
 def count_down(count):
-    window.after(1000,count_down,count -1 )
+    count_min = math.floor(count /  60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text,text=f'{count_min}:{count_sec}')
+    if count > 0 :
+        window.after(1000,count_down,count -1 )
 
-count_down(5)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -34,14 +42,15 @@ window.config(padx=100, pady=20,bg=YELLOW)
 canvas = Canvas(width=200, height=255,bg=YELLOW,highlightthickness=0)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas.create_image(100,112,image=tomato_img )
-canvas.create_text(103,130 , text = "00:00",fill='white' ,font=(FONT_NAME , 35 ,'bold'))
+timer_text = canvas.create_text(103,130 , text = "00:00",fill='white' ,font=(FONT_NAME , 35 ,'bold'))
 canvas.create_text(103,20 , text = "Timer",fill=GREEN ,font=(FONT_NAME , 35 ,'bold'))
 canvas.create_text(103,280 , text = TICK,fill=GREEN ,font=(FONT_NAME , 20 ,'bold'))
 
 
+
 canvas.pack()
 
-start = Button(window, text = "Start",bg='white')
+start = Button(window, text = "Start",bg='white' ,command=start_timer)
 finish = Button(window, text = "Finish",bg='white')
 start.pack(side='left')
 finish.pack(side='right')

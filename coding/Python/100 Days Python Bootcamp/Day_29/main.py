53833495e3f3c661d3f9a057a7f8e9b87bdddc3e
hossein <h0ssein2011@yahoo.com>
2023-03-29 18:05:11 +0300

    Add Entry and message boxes

--- coding/Python/100 Days Python Bootcamp/Day_29/main.py
+++ coding/Python/100 Days Python Bootcamp/Day_29/main.py
@@ -1,8 +1,6 @@
 from tkinter import *
+from tkinter import messagebox
 
-window = Tk()
-window.title('password manager')
-window.config(padx=50, pady=50)
 
 
 
@@ -12,11 +10,36 @@ window.config(padx=50, pady=50)
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0  or len(password) == 0:
+        messagebox.showinfo(title="Opps",message="Please dont leave blank eny entry")
+    else :
+
+        is_ok = messagebox.askokcancel(title='Confirmation' , 
+                           message=f'these are the details of the information :{website} \n {email} \n {password}')
+        if is_ok:
+            with open('data.txt','a') as  f:
+                
+                f.write(f'{website} | {email} | {password}')
+                website_entry.delete(0,END)
+                password_entry.delete(0,END)
+
+                f.write('\n')
+        
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('password manager')
+window.config(padx=50, pady=50)
 
 #add image
-canvas = Canvas(width=300, height=200,highlightthickness=0)
+canvas = Canvas(width=200, height=200,highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100,100,image=logo_img )
 canvas.grid(column=1, row=0)
@@ -33,6 +56,7 @@ email_label = Label(text="Email/username")
 email_label.grid(column=0, row=2)
 
 email_entry = Entry(width=35)
+email_entry.insert(0,'hossein@gmail.com')
 email_entry.grid(column=1, row=2,columnspan=2)
 
 password_label = Label(text="Password")
@@ -45,7 +69,7 @@ gen_pass = Button(window, text = "Generate Password")
 gen_pass.grid(column=2, row=3)
 
 
-add_button = Button(window, text = "Add",bg='white',width=36)
+add_button = Button(window, text = "Add",bg='white',width=36,command=save)
 add_button.grid(column=1, row=4,columnspan=2)
 
 
